5174fcf4aa624692f8466d3ea976a93dd2b7ffaf
Joel Ladeda <ladedajoel.1994@gmail.com>
2018-09-11 17:30:47 -0300

    Rectangulo , Triangulo y Circulo completos

--- TpFormas/src/triangulo/TrianguloTest.java
+++ TpFormas/src/triangulo/TrianguloTest.java
@@ -8,7 +8,41 @@ public class TrianguloTest {
 
 	@Test
 	public void testCrearTriangulo() {
-		fail("Not yet implemented");
+		
+		Triangulo unTriangulo;
+		unTriangulo = new Triangulo(10.0, 8.0, 8.0, 12.0);
+		assertNotNull(unTriangulo);
+	
+		Triangulo otroTriangulo;
+		otroTriangulo = new Triangulo(5.0, 4.0, 4.0, 6.0);
+		assertNotNull(otroTriangulo);
+		
 	}
 
+	@Test
+	public void testPerimetroTriangulo() {
+	
+		Triangulo unTriangulo;
+		unTriangulo = new Triangulo(10.0, 8.0, 8.0, 12.0);
+		Double esperado = 26.0;
+		Double actual = unTriangulo.calcularPerimetroTriangulo();
+		assertEquals(esperado, actual);
+		
+	}
+	
+	@Test
+	public void testAreaTriangulo() {
+		
+		Triangulo otroTriangulo;
+		otroTriangulo = new Triangulo(5.0, 4.0, 4.0, 6.0);
+		Double esperado = 15.0;
+		Double actual = otroTriangulo.calcularAreaTriangulo();
+		assertEquals(esperado, actual);
+		
+	}
+	
+	
+	
+	
+	
 }
